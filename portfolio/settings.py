3b63fa28ca9ea71a7db2d287a800652d52c5fab9
duncan <duncankivindyo@gmail.com>
2025-10-13 13:17:34 +0300

    Fix database config4 for Render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-25xz7%ujy5j0zc334sl9=yjiv*1l+rn*du3)_yj5-wieoee^lx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfoli-1-4aqj.onrender.com']
+ALLOWED_HOSTS = ['portfoli-1-4aqj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
